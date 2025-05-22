b4d759b0998814ac1459bc06be73c94358e07da1
nicolasult <nicolas.ultrillo@gmail.com>
2025-05-22 21:55:43 +0200

    Add player class and display the player

--- code/main.py
+++ code/main.py
@@ -1,24 +1,35 @@
 from settings import *
+from player import Player
 
 class Game:
     def __init__(self):
         pygame.init()
-        self.display_surface = pygame.display.set_mode(WINDOW_WIDTH, WINDOW_HEIGHT)
+        self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
         pygame.display.set_caption("Survivor")
         self.clock = pygame.time.Clock()
         self.running = True
+
+        self.all_sprites = pygame.sprite.Group()
+
+        self.player = Player((400, 300), self.all_sprites)
+
 
     def run(self):
         while self.running:
             dt = self.clock.tick() / 1000
         
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                self.running = False
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
             
+            self.all_sprites.update(dt)
+
+            self.all_sprites.draw(self.display_surface)
             pygame.display.update()
+
         pygame.quit()
 
-game = Game()
-game.run
+if __name__ == "__main__":
+    game = Game()
+    game.run()
 
