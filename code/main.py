5e9a5f6a2ddbf4d0c10d29f6c3fb8918bc154e30
nicolasult <nicolas.ultrillo@gmail.com>
2025-05-24 18:18:22 +0200

    Finish camera

--- code/main.py
+++ code/main.py
@@ -26,7 +26,6 @@ class Game:
 
         # Sprites
 
-        self.player = Player((500, 300), self.all_sprites, self.collision_sprites)
         
     def setup(self):
         map = load_pygame(join("data", "maps", "world.tmx"))
@@ -36,6 +35,9 @@ class Game:
             CollisionSprite((obj.x, obj.y), obj.image, (self.all_sprites, self.collision_sprites))
         for obj in map.get_layer_by_name("Collisions"):
             CollisionSprite((obj.x, obj.y), pygame.Surface((obj.width, obj.height)), self.collision_sprites)
+        for obj in map.get_layer_by_name("Entities"):
+            if obj.name == "Player":
+                self.player = Player((obj.x, obj.y), self.all_sprites, self.collision_sprites)
 
     def run(self):
         while self.running:
@@ -57,7 +59,7 @@ class Game:
             # Draw
 
             self.display_surface.fill("Black")
-            self.all_sprites.draw()
+            self.all_sprites.draw(self.player.rect.center)
             pygame.display.update()
 
         pygame.quit()
